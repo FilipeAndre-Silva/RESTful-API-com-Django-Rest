dad8aaf464a6323447e755546f24675f9f6bab19
Filipe André da Silva <filipeandre.private@gmail.com>
2021-12-06 09:58:28 -0300

    Implementando exemplo do curso de Rest Api com DRF.

---
+++ agendamentos/api/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+from agendamentos.models import Agendamentos
+from historicos.api.serializers import HistoricosSerializer, HistoricosDetalhesSerializer
+
+
+class AgendamentosSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Agendamentos
+        fields = '__all__'
+
+
+class AgendamentosDetalhesSerializer(serializers.ModelSerializer):
+
+    historicos = HistoricosDetalhesSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Agendamentos
+        fields = (
+            'id_agendamento',
+            'data_hora',
+            'data_criacao',
+            'cancelado',
+            'obs',
+            'tipo',
+            'id_paciente',
+            'historicos',
+        )
